3b7126953af4cd5d915c976c43494d61a7dac978
Jonny Townend <jonny@brightec.co.uk>
2024-02-10 13:53:23 +0000

    Show wait time error on an assistant message

--- src/components/message-bubble/message-bubble.component.tsx
+++ src/components/message-bubble/message-bubble.component.tsx
@@ -5,8 +5,9 @@ import { format } from "../util/formatting"
 import { MessageLoading } from "../message-loading/message-loading.component"
 import { CopyButton } from "../copy-button/copy-button.component"
 
-const Container = styled.div`
+const Container = styled.div<{ isAssistant: boolean }>`
     position: relative;
+    margin-left: ${p => p.isAssistant ? 32 : 0}px;
 `
 
 const Bubble = styled.div<{ isAssistant: boolean }>`
@@ -14,13 +15,22 @@ const Bubble = styled.div<{ isAssistant: boolean }>`
     padding-right: 24px;
     background-color: ${p => p.isAssistant ? '#E6F7FF' : '#F0F0F0'};
     border-radius: 8px;
-    margin-left: ${p => p.isAssistant ? 32 : 0}px;
+`
+
+const FooterContainer = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding-top: 4px;
+`
+
+const MessageContainer = styled.div`
+    font-size: 12px;
+    color: grey;
 `
 
 const ButtonsContainer = styled.div`
     display: flex;
-    justify-content: flex-end;
-    padding-top: 4px;
 `
 
 const ActionButton = styled.div`
@@ -28,13 +38,6 @@ const ActionButton = styled.div`
     cursor: pointer;
     font-size: 20px;
     margin-left: 8px;
-`
-
-const TickButtonContainer = styled.div<{ show: boolean }>`
-    position: absolute;
-    top: 0;
-    opacity: ${p => p.show ? 1 : 0};
-    transition: opacity 0.5s;
 `
 
 interface MessageBubbleProps {
@@ -47,20 +50,50 @@ interface MessageBubbleProps {
 }
 
 export const MessageBubble = ({ message, role, isPartial, isLast, onStopClicked, onRetryClicked }: MessageBubbleProps) => {
+    const [showWaitingMessage, setShowWaitingMessage] = useState(false)
     const isAssistant = role === "assistant"
     const messageHasContent = useMemo(() => message.trim() !== '', [message])
 
+    useEffect(() => {
+        if (messageHasContent) {
+            setShowWaitingMessage(false)
+            return
+        }
+
+        const timeout = setTimeout(() => {
+            if (messageHasContent) {
+                return
+            }
+
+            setShowWaitingMessage(true)
+        }, 4000)
+
+        return () => clearTimeout(timeout)
+    }, [messageHasContent, setShowWaitingMessage])
+
     return (
-        <Container>
+        <Container isAssistant={isAssistant}>
             <Bubble isAssistant={isAssistant}>
                 {isAssistant && !messageHasContent ? <MessageLoading /> : format(message)}
             </Bubble>
-            {isAssistant && !isPartial && messageHasContent && isLast && (
-                <ButtonsContainer>
-                    <CopyButton copyText={message} />
-                    <ActionButton onClick={onRetryClicked}>🔄</ActionButton>
-                </ButtonsContainer>
-            )}
+            <FooterContainer>
+                {showWaitingMessage && (
+                    <MessageContainer>
+                        <em>
+                            The model is taking longer than expected to respond.<br />
+                            This can happen when you first run the model, or if your machine doesn't have enough RAM.<br />
+                            If nothing happens for a while, try a different model.
+                        </em>
+                    </MessageContainer>
+                )}
+                <div />
+                {isAssistant && !isPartial && messageHasContent && isLast && (
+                    <ButtonsContainer>
+                        <CopyButton copyText={message} />
+                        <ActionButton onClick={onRetryClicked}>🔄</ActionButton>
+                    </ButtonsContainer>
+                )}
+            </FooterContainer>
         </Container>
     )
 }
