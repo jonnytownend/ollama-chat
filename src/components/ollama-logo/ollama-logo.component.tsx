11489e43fe151a65014d3cdfc208857502b8ff8c
Jonny Townend <jonny@brightec.co.uk>
2024-02-13 00:11:51 +0000

    Update and add app icons

--- src/components/ollama-logo/ollama-logo.component.tsx
+++ src/components/ollama-logo/ollama-logo.component.tsx
@@ -1,7 +1,10 @@
+import styled from "styled-components"
+
 interface OllamaLogoProps { 
     size?: number
 }
 
 export const OllamaLogo = ({ size = 24 }: OllamaLogoProps) => (
-    <img src="https://ollama.com/public/ollama.png" width={size} />
+    // <img src="https://ollama.com/public/ollama.png" width={size} />
+    <p style={{ fontSize: `${size}px`, transform: 'scale(1.5)'}}>🦙</p>
 )
